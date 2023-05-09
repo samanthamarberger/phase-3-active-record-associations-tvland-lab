ff5041b1cb811c2deb7f789373f6e25a08361ea2
samanthamarberger <slatham04@gmail.com>
2023-05-09 12:23:12 -0600

    commit tree 9589f0a39c364b7fce18cf3a072b25509d6e401e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
+require "pry"
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.map(&:full_name)
+  end
 end
