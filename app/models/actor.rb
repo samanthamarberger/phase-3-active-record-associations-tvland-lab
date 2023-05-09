628879c050232e0614a2959504e17bfa0000fc3d
samanthamarberger <slatham04@gmail.com>
2023-05-09 12:24:46 -0600

    DONE

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
+require "pry"
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        return "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        # binding.pry
+        roles = []
+        characters = self.characters
+        characters.each do |c|
+            roles << "#{c.name} - #{c.show.name}"
+        end
+        return roles.join("\n")
+    end
 end
